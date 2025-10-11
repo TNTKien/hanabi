f2ca639b81129d889f6109c1aa0ca79472f404c8
iamneyk <iamneyk@gmail.com>
2025-10-12 02:09:31 +0700

    init

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import { Hono } from "hono";
+import { Button, Components, DiscordHono } from "discord-hono";
 
-const app = new Hono<{ Bindings: CloudflareBindings }>();
-
-app.get("/message", (c) => {
-  return c.text("Hello Hono!");
-});
+const app = new DiscordHono()
+  .command("hello", (c) => c.res(`Hello, ${c.var.name ?? "World"}!`))
+  .command("help", (c) =>
+    c.res({
+      components: new Components().row(
+        new Button("https://discord-hono.luis.fun", ["📑", "Docs"], "Link"),
+        new Button("delete", ["🗑️", "Delete"])
+      ),
+    })
+  )
+  .component("delete", (c) => c.update().resDefer((c) => c.followup()));
 
 export default app;
+
+// Example to check next
+// https://github.com/luisfun/discord-hono-examples/tree/main/workerd-use-factory
